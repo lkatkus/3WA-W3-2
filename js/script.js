0ce917f3c57b8f6d8bf05961e20c7a68ac226ed6
lkatkus <laimonas@katkus.eu>
2018-01-09 12:57:40 +0200

    Circular reference protection

--- js/script.js
+++ js/script.js
@@ -7,39 +7,73 @@ function liekana(dalinys,daliklis){
     return liekana(dalinys-daliklis, daliklis);
 }
 
+var car2;
+
 var car1 = {
-    // gamintojas: "Mazda",
-    // spalva: "Pilka",
-    // metai: 2005,
+    gamintojas: "Mazda",
+    spalva: "Pilka",
+    metai: 2005,
     variklis:{
         kuras:"Benzinas",
         turis:1.6
     },
     muzika:{
-        cd: "Yra",
-        usb: "Nera"
+        cd:"yra",
+        usb:"nera"
     },
-    keleiviai:["Petras", "Jonas", "Aiste", "Ona"]
+    keleiviai:["Jonas","Petras","Ona"]
 }
 
-function print(p){
+car1.circle = car1;
+
+function print(p,path){
+    var top = false;
+    if (!path) {
+        top = true;
+    }
     if(typeof p === "string" || typeof p === "number" || typeof p === "boolean" || typeof p === "undefined" || p === null){
-        return p;
+        console.log(p);
+
     }else if(typeof p === "function"){
-        return (p.toString());
+        console.log (p.toString());
+
     }else if(typeof p === "object"){
-        if(Array.isArray(p)){
-            return p;
-            for(var i = 0; i < p.length; i++){
-                console.log(i);
-                print(p[i]);
+        if(!Array.isArray(path)){ /* circular reference prevention while saving depth of path */
+            path = [];
+            console.log("!!! PATH CREATED !!!");
+        }else{
+            for(var i = 0; i < path.length; i++){ /* callback function provides back a "path" of object into check array */
+                if(path[i] === p){ /* checks if there is the same object in the path */
+                    console.log("!!! CIRCULAR REFFERENCE !!!");
+                    return;
+                }
+                console.log("=== ENTERING OBJECT ===");
             }
+        }
+
+        path.push(p);
+        console.log(path);
+
+        if(Array.isArray(p)){
+            console.log("=== ENTERING ARRAY ===");
+            for(var i = 0; i < p.length; i++){
+                print(p[i], path);
+            }
+            console.log("=== EXIT ARRAY ===");
+
         }else{
                 for(var key in p){
-                    console.log(key + " - " + print(p[key]));
+                    console.log(key);
+                    print(p[key], path);
                 }
-            }
+            console.log("=== EXIT OBJECT ===");
+        }
+        path.pop();
+
     }else{
         console.log("symbol");
+    }
+    if (top) {
+        console.log("FINAL");
     }
 }
